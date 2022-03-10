d38afc6dfe5f1aea5ee6ea08cd20b7b3922772b3
Millie Clare <Millie_Clare@outlook.com>
2022-03-10 15:27:25 +0000

    write pdf to new txt file. Remove stop words before writing

--- pdfReader.js
+++ pdfReader.js
@@ -1,4 +1,8 @@
 const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
+const fs = require("fs");
+const stopwords = require("stopwords-en");
+
+const fileToRead = "./ABN_AMRO_2015_External_review_report.pdf";
 
 function pdfToText(url, separator = " ") {
   let pdf = pdfjsLib.getDocument(url);
@@ -15,6 +19,7 @@ function pdfToText(url, separator = " ") {
           return textContent.then(function (text) {
             // return content promise
             return text.items
+
               .map(function (obj) {
                 return obj.str;
               })
@@ -33,12 +38,29 @@ function pdfToText(url, separator = " ") {
   });
 }
 
-// example of use:
-// waiting on pdfToText to finish completion, or error
-pdfToText("./ABN_AMRO_2015_External_review_report.pdf").then(
+function removeStopwords(text) {
+  const res = [];
+  const words = text.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    let checkWord = words[i].split(".").join("");
+    if (!stopwords.includes(checkWord)) {
+      res.push(checkWord);
+    }
+  }
+  return res.join(" ");
+}
+
+pdfToText(fileToRead).then(
   function (pdfTexts) {
-    console.log(pdfTexts);
-    // RESULT: ['TEXT-OF-PAGE-1', 'TEXT-OF-PAGE-2', ...]
+    const txtFileName = fileToRead.split(".")[1];
+    pdfTexts = pdfTexts.toString();
+    pdfTexts = removeStopwords(pdfTexts);
+    fs.writeFile(`./${txtFileName}.txt`, pdfTexts.toString(), (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
   },
   function (reason) {
     console.error(reason);
