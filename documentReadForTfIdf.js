c80bceb9b855327e44119444cc3c3a0236819e22
Millie Clare <Millie_Clare@outlook.com>
2022-06-20 23:16:48 +0100

    cleaned up functions and added in stemming

--- documentReadForTfIdf.js
+++ documentReadForTfIdf.js
@@ -7,26 +7,26 @@ const tfidf = new TfIdf();
 const filesToRead = fs.readdirSync("./fileOutputs");
 
 const loadDocuments = (listOfFilesToRead, tfidfObject) => {
-  listOfFilesToRead.forEach((file) => {
-    try {
-      const data = fs.readFileSync(`./fileOutputs/${file}`, "utf8");
-      tfidfObject.addDocument(data);
-      // console.log(data);
-    } catch (err) {
-      console.error(err);
-    }
-  });
+    listOfFilesToRead.forEach((file) => {
+        try {
+            const data = fs.readFileSync(`./fileOutputs/${file}`, "utf8");
+            tfidfObject.addDocument(data);
+        } catch (err) {
+            console.error(err);
+        }
+    });
 };
 
 const runTfIdf = (tfidfObject) => {
-  tfidfObject.tfidfs("green", function (i, measure) {
-    console.log("document #" + i + " is " + measure);
-  });
+    tfidfObject.tfidfs("green", function(i, measure) {
+        console.log("document #" + i + " has " + measure);
+    });
 };
 
 const analyseDocumentScores = (documentsToAnalyse, tfidfObject) => {
-  loadDocuments(documentsToAnalyse, tfidfObject);
-  runTfIdf(tfidfObject);
+    loadDocuments(documentsToAnalyse, tfidfObject);
+
+    runTfIdf(tfidfObject);
 };
 
 analyseDocumentScores(filesToRead, tfidf);
--- pdfReader.js
+++ pdfReader.js
@@ -1,79 +1,88 @@
 const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
 const fs = require("fs");
 const stopwords = require("stopwords-en");
+const natural = require("natural");
 
 // list files in a directory
 const filesToRead = fs.readdirSync("./corpus");
 
-function pdfToText(url, separator = " ") {
-  let pdf = pdfjsLib.getDocument(url);
-  return pdf.promise.then(function (pdf) {
-    // get all pages text
-    let maxPages = pdf._pdfInfo.numPages;
-    let countPromises = []; // collecting all page promises
-    for (let i = 1; i <= maxPages; i++) {
-      let page = pdf.getPage(i);
-      countPromises.push(
-        page.then(function (page) {
-          // add page promise
-          let textContent = page.getTextContent();
-          return textContent.then(function (text) {
-            // return content promise
-            return text.items
+const pdfToText = (url, separator = " ") => {
+    let pdf = pdfjsLib.getDocument(url);
+    return pdf.promise.then(function(pdf) {
+        // get all pages text
+        let maxPages = pdf._pdfInfo.numPages;
+        let countPromises = []; // collecting all page promises
+        for (let i = 1; i <= maxPages; i++) {
+            let page = pdf.getPage(i);
+            countPromises.push(
+                page.then(function(page) {
+                    // add page promise
+                    let textContent = page.getTextContent();
+                    return textContent.then(function(text) {
+                        // return content promise
+                        return text.items
 
-              .map(function (obj) {
-                return obj.str;
-              })
-              .join(separator); // value page text
-          });
-        })
-      );
-    }
-    // wait for all pages and join text
-    return Promise.all(countPromises).then(function (texts) {
-      for (let i = 0; i < texts.length; i++) {
-        texts[i] = texts[i].replace(/\s+/g, " ").trim();
-      }
-      return texts;
+                            .map(function(obj) {
+                                return obj.str;
+                            })
+                            .join(separator); // value page text
+                    });
+                })
+            );
+        }
+        // wait for all pages and join text
+        return Promise.all(countPromises).then(function(texts) {
+            for (let i = 0; i < texts.length; i++) {
+                texts[i] = texts[i].replace(/\s+/g, " ").trim();
+            }
+            return texts;
+        });
     });
-  });
-}
+};
 
-function removeStopwords(text) {
-  const res = [];
-  const words = text.split(" ");
-  for (let i = 0; i < words.length; i++) {
-    let checkWord = words[i].split(".").join("");
-    if (!stopwords.includes(checkWord)) {
-      res.push(checkWord);
+const removeStopwords = (text) => {
+    const res = [];
+    const words = text.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        let checkWord = words[i].split(".").join("");
+        if (!stopwords.includes(checkWord)) {
+            res.push(checkWord);
+        }
     }
-  }
-  return res.join(" ");
-}
+    return res.join(" ");
+};
+
+const stemOutput = (text) => {
+    const stemmer = natural.LancasterStemmer;
+    const stemmedText = [...new Set(stemmer.tokenizeAndStem(text))];
+    return stemmedText.join(" ");
+};
 
 const updateTextFiles = (listOfFiles) => {
-  listOfFiles.forEach((file) => {
-    pdfToText(`./corpus/${file}`).then(
-      function (pdfTexts) {
-        const txtFileName = file.split(".")[0];
-        pdfTexts = pdfTexts.toString();
-        pdfTexts = removeStopwords(pdfTexts);
-        fs.writeFile(
-          `./fileOutputs/${txtFileName}.txt`,
-          pdfTexts.toString(),
-          (err) => {
-            if (err) {
-              console.error(err);
-              return;
+    listOfFiles.forEach((file) => {
+        pdfToText(`./corpus/${file}`).then(
+            function(pdfTexts) {
+                const txtFileName = file.split(".")[0];
+                pdfTexts = pdfTexts.toString();
+                pdfTexts = removeStopwords(pdfTexts);
+                pdfTexts = stemOutput(pdfTexts);
+                console.log(pdfTexts);
+                fs.writeFile(
+                    `./fileOutputs/${txtFileName}.txt`,
+                    pdfTexts.toString(),
+                    (err) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                    }
+                );
+            },
+            function(reason) {
+                console.error(reason);
             }
-          }
         );
-      },
-      function (reason) {
-        console.error(reason);
-      }
-    );
-  });
+    });
 };
 
 updateTextFiles(filesToRead);
