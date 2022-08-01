c95d1cc640da5b22071a8c11ab9bdbd1eaa5bfe1
Millie Clare <Millie_Clare@outlook.com>
2022-08-02 00:45:30 +0100

    made mongo work

--- expressEsgApp/mongo/mongo-db.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:8081/esgdb";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
-});
