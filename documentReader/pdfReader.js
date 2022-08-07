85a8eb0e27d8cfd36d1048be08fc57570f7c0a43
Millie Clare <Millie_Clare@outlook.com>
2022-08-07 23:52:46 +0100

    added in logic to get file urls, download them and write them to .txt files

--- documentReader/pdfReader.js
+++ documentReader/pdfReader.js
@@ -1,10 +1,63 @@
 const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
+// import pdfjsLib from "pdfjs-dist/legacy/build/pdf.js";
+// import fs from "fs";
+// import http from "http";
+// import https from "https";
+// import stopwords from "stopwords-en";
 const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const stopwords = require("stopwords-en");
 const natural = require("natural");
 
 // list files in a directory
-const filesToRead = fs.readdirSync("./corpus");
+const filesToRead = fs.readdirSync("../corpus");
+// import pdfsAsJson from "../excel_pdfs/ICMA-Sustainable-Bonds-Database-110322";
+const pdfsAsJson = require("../excel_pdfs/ICMA-Sustainable-Bonds-Database-110322.json");
+
+const getPdfs = (pdfs) => {
+    pdfs.forEach((pdf) => {
+        if (
+            pdf.hasOwnProperty("External_Review_Report_Hyperlink_1") ||
+            pdf.hasOwnProperty("External_Review_Report_Hyperlink")
+        ) {
+            fileName = pdf.Green_Bond_Issuer;
+            fileLink =
+                pdf.External_Review_Report_Hyperlink_1 ||
+                pdf.External_Review_Report_Hyperlink;
+
+            downloadPdfs(fileLink, fileName);
+        }
+    });
+};
+
+const downloadPdfs = (fileUrl, fileName) => {
+    fileName = fileName.replace(/[^a-zA-Z0-9-().]/g, "");
+    const isHttp = fileUrl.split(":");
+    if (isHttp[0] === "http") {
+        http.get(fileUrl, (res) => {
+            const path = `../corpus/${fileName}.pdf`;
+            const writeStream = fs.createWriteStream(path);
+            res.pipe(writeStream);
+            writeStream.on("finish", () => {
+                writeStream.close();
+                console.log("------------Download Completed!");
+            });
+        });
+    } else if (isHttp[0] === "https") {
+        https.get(fileUrl, (res) => {
+            const path = `../corpus/${fileName}.pdf`;
+            const writeStream = fs.createWriteStream(path);
+            res.pipe(writeStream);
+            writeStream.on("finish", () => {
+                writeStream.close();
+                console.log("------------Download Completed!");
+            });
+        });
+    } else {
+        console.log("Error, file is neither http or https", fileName);
+    }
+};
 
 const pdfToText = (url, separator = " ") => {
     let pdf = pdfjsLib.getDocument(url);
@@ -21,7 +74,6 @@ const pdfToText = (url, separator = " ") => {
                     return textContent.then(function(text) {
                         // return content promise
                         return text.items
-
                             .map(function(obj) {
                                 return obj.str;
                             })
@@ -40,35 +92,38 @@ const pdfToText = (url, separator = " ") => {
     });
 };
 
-const removeStopwords = (text) => {
-    const res = [];
-    const words = text.split(" ");
-    for (let i = 0; i < words.length; i++) {
-        let checkWord = words[i].split(".").join("");
-        if (!stopwords.includes(checkWord)) {
-            res.push(checkWord);
-        }
-    }
-    return res.join(" ");
-};
+// const removeStopwords = (text) => {
+//     const res = [];
+//     const words = text.split(" ");
+//     for (let i = 0; i < words.length; i++) {
+//         let checkWord = words[i].split(".").join("");
+//         if (!stopwords.includes(checkWord)) {
+//             res.push(checkWord);
+//         }
+//     }
+//     return res.join(" ");
+// };
 
-const stemOutput = (text) => {
-    const stemmer = natural.LancasterStemmer;
-    const stemmedText = [...new Set(stemmer.tokenizeAndStem(text))];
-    return stemmedText.join(" ");
-};
+// const stemOutput = (text) => {
+//     const stemmer = natural.LancasterStemmer;
+//     console.log("i am waking up to the sounds of chainsaws".tokenizeAndStem());
+//     const stemmedText = [...new Set(stemmer.tokenizeAndStem(text))];
+//     return stemmedText.join(" ");
+// };
 
 const updateTextFiles = (listOfFiles) => {
     listOfFiles.forEach((file) => {
-        pdfToText(`./corpus/${file}`).then(
+        pdfToText(`../corpus/${file}`).then(
             function(pdfTexts) {
                 const txtFileName = file.split(".")[0];
                 pdfTexts = pdfTexts.toString();
-                pdfTexts = removeStopwords(pdfTexts);
-                pdfTexts = stemOutput(pdfTexts);
-                console.log(pdfTexts);
+                // console.log(countWords(pdfTexts));
+
+                // pdfTexts = removeStopwords(pdfTexts);
+                // pdfTexts = stemOutput(pdfTexts);
+                // console.log(pdfTexts);
                 fs.writeFile(
-                    `./fileOutputs/${txtFileName}.txt`,
+                    `../fileOutputs/${txtFileName}.txt`,
                     pdfTexts.toString(),
                     (err) => {
                         if (err) {
@@ -84,5 +139,7 @@ const updateTextFiles = (listOfFiles) => {
         );
     });
 };
+
+// getPdfs(pdfsAsJson);
 
 updateTextFiles(filesToRead);
