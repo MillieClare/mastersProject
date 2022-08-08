36a2627eb5b9ae8efd56ecd8b973a761cedaa203
Millie Clare <Millie_Clare@outlook.com>
2022-08-08 21:47:29 +0100

    made js files into components

--- documentReader/documentReadForTfIdf.js
+++ documentReader/documentReadForTfIdf.js
@@ -3,30 +3,32 @@ const natural = require("natural");
 const TfIdf = natural.TfIdf;
 const tfidf = new TfIdf();
 
-// list all files in directory
-const filesToRead = fs.readdirSync("../fileOutputs");
+const DocumentReadForTfIdf = () => {
+  // list all files in directory
+  const filesToRead = fs.readdirSync("../fileOutputs");
 
-const loadDocuments = (listOfFilesToRead, tfidfObject) => {
+  const loadDocuments = (listOfFilesToRead, tfidfObject) => {
     listOfFilesToRead.forEach((file) => {
-        try {
-            const data = fs.readFileSync(`../fileOutputs/${file}`, "utf8");
-            tfidfObject.addDocument(data);
-        } catch (err) {
-            console.error(err);
-        }
+      try {
+        const data = fs.readFileSync(`../fileOutputs/${file}`, "utf8");
+        tfidfObject.addDocument(data);
+      } catch (err) {
+        console.error(err);
+      }
     });
-};
+  };
 
-const runTfIdf = (tfidfObject) => {
-    tfidfObject.tfidfs("climate", function(i, measure) {
-        console.log("document #" + i + " has " + measure);
+  const runTfIdf = (tfidfObject) => {
+    tfidfObject.tfidfs("climate", function (i, measure) {
+      console.log("document #" + i + " has " + measure);
     });
-};
+  };
 
-const analyseDocumentScores = (documentsToAnalyse, tfidfObject) => {
+  const analyseDocumentScores = (documentsToAnalyse, tfidfObject) => {
     loadDocuments(documentsToAnalyse, tfidfObject);
 
     runTfIdf(tfidfObject);
+  };
 };
 
-analyseDocumentScores(filesToRead, tfidf);
+export default DocumentReadForTfIdf;
