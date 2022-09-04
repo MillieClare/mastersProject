d05ecc65509ed9e4c1a0ac2aa4094e637c13d180
Millie Clare <Millie_Clare@outlook.com>
2022-09-04 22:22:03 +0100

    started some more normalisation

--- GreenBookBE/src/functions/documentReader/normaliseData.ts
+++ GreenBookBE/src/functions/documentReader/normaliseData.ts
@@ -3,22 +3,36 @@ import wordFrequencies from './word_frequencies.json';
 const sectors = new Set(wordFrequencies.map((element) => element.sector));
 const sectorsArray = Array.from(sectors);
 
-const getMaximumFromSector = () => {
-  sectorsArray.map((sector) => {
-    const reports = wordFrequencies.filter((record) => record.sector === sector);
-    console.log('=====================', reports.length);
-    let allWordsArray: any = [];
-    reports.map((entry) => {
-      allWordsArray = allWordsArray.concat([entry.frequentWords]);
+const dataObj = {
+  // data from sectors
+};
+
+const range = [0, 1];
+
+const normaliseObject = (data: any, range: any) => {
+  let minMax: any = { min: null, max: null };
+  Object.keys(data).forEach((key) => {
+    const values: any = Object.values(data[key]);
+    const min = Math.min.apply(Math, values);
+    const max = Math.max.apply(Math, values);
+    if (min < minMax.min || minMax.min === null) {
+      minMax.min = min;
+    }
+
+    if (max > minMax.max || minMax.max === null) {
+      minMax.max = max;
+    }
+  });
+  const variation = (range[1] - range[0]) / (minMax.max - minMax.min);
+
+  Object.keys(data).forEach((key) => {
+    Object.keys(data[key]).forEach((el) => {
+      const val = (range[0] + (data[key][el] - minMax.min) * variation).toFixed(2);
+      data[key][el] = +val;
     });
-    console.log(allWordsArray);
   });
 };
 
-const getMinimumFromSector = () => {};
+normaliseObject(dataObj, range);
 
-function getObjKey(obj: any, value: string) {
-  return Object.keys(obj).find((key) => obj[key] === value);
-}
-
-getMaximumFromSector();
+console.log(dataObj);
